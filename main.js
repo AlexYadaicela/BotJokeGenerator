18fc398db18f65328029917971de1b23e70596a7
spycrab <wialex121@gmail.com>
2024-11-19 21:35:08 -0500

    modified files

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const btnJokeGenerator = document.getElementById('joke_generator'); 
 const synth = window.speechSynthesis; 
 const voices = synth.getVoices(); 
-console.log(voices); 
+let beginAnimation; 
 
 async function getData(){
     const url = 'https://v2.jokeapi.dev/joke/Any?lang=en&type=twopart'; 
@@ -12,7 +12,8 @@ async function getData(){
         }
         const json = await response.json(); 
         arrangeJoke(json['setup'], json['delivery']);     
-        textToSpeach(json['setup'], json['delivery']);   
+        textToSpeach(json['setup'], json['delivery']);  
+        beginAnimation = setInterval(animateBot, 850); 
     }catch(error){
         console.log(error.message); 
     }
@@ -28,16 +29,37 @@ function arrangeJoke(setup, delivery){
 
 function textToSpeach(setup, delivery){
     const utterThis = new SpeechSynthesisUtterance(setup + ' ' + delivery);
-    utterThis.voice = voices[1]; 
+    utterThis.voice = voices[1];  
     synth.speak(utterThis); 
+}
+//used to determine current sprite
+let curr = 0; 
+const sprite = ['closed-mouth-man.png', 'open-mouth-man.png'];
+
+function animateBot(){
+    let imgElement = document.querySelector("img"); 
+    switch(curr){
+        case 0:
+            imgElement.src = sprite[0];
+            curr++; 
+            break; 
+        case 1: 
+            imgElement.src = sprite[1]; 
+            curr--; 
+            break; 
+        default:
+            imgElement.src = sprite[0]; 
+    }
 }
 
 document.addEventListener('keydown', (e) => {
     if(e.key === 'j' || e.key === 'J'){
+        clearInterval(beginAnimation);         
         getData(); 
     }
 });
 
 btnJokeGenerator.addEventListener('click', () => {
-    getData();     
+    clearInterval(beginAnimation); 
+    getData();    
 });
